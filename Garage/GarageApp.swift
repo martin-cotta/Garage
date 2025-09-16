9df5727084cec2ef176b73bd7b83e81c26d19a61
Martin Cotta <martin.cotta@gmail.com>
2025-09-16 16:38:07 -0700

    Initial Commit

---
+++ Garage/GarageApp.swift
@@ -0,0 +1,17 @@
+//
+//  GarageApp.swift
+//  Garage
+//
+//  Created by Martin on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct GarageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
